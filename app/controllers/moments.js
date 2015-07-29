8a27bacc96d453feeac148cf98382a6e344e71ea
Becca <rebecca.vasil@gmail.com>
2015-07-29 11:55:50 -0600

    added flash message when moment was successfuly posted

--- app/controllers/moments.js
+++ app/controllers/moments.js
@@ -9,10 +9,12 @@ export default Ember.ObjectController.extend({
       var location = controller.get('locationCopy');
       var happenedAt = controller.get('happenedAtCopy');
 
-      if (content && location) {
+      if (content && location && happenedAt) {
         var moment = controller.store.createRecord('moment', { content: content, caption: caption,
             location: location, happenedAt: happenedAt });
-        moment.save();
+        moment.save().then(function(){
+          controller.get('flashes').success('Your moment was successfully posted!');
+        }.bind(controller));
       }
       controller.set('contentCopy', '');
       controller.set('captionCopy', '');
